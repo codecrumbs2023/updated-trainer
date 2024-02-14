b6c17f6b86944d30d988d2b5e41cb2b332036d06
pracheetakalra <pracheetakalra@gmail.com>
2024-02-14 11:04:06 +0530

    updated styling for graph

--- HiringPlatform/src/pages/admin/RevenueAnalysisPage.jsx
+++ HiringPlatform/src/pages/admin/RevenueAnalysisPage.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Bar } from "react-chartjs-2";
-//import Chart from "chart.js/auto";
+import Chart from "chart.js/auto";
  
 function RevenueAnalysisPage() {
   const [quarterlyRevenue, setQuarterlyRevenue] = useState([]);
